29d2adcc447f177cf13e41a74ff148755335768b
tmtabor <tabor@broadinstitute.org>
2019-03-20 17:18:30 -0400

    Adding services API config

--- notebook-repository/start-repository.sh
+++ notebook-repository/start-repository.sh
@@ -4,4 +4,4 @@
 docker network create repo
 
 # Start the Notebook Repository container
-docker run --rm --net repo -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock genepattern/notebook-repository
+docker run --rm --net repo -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock genepattern/notebook-repository
