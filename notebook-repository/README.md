e2c773b176df5aaafdca227302abd814e129f4ea
tmtabor <tabor@broadinstitute.org>
2019-04-05 12:36:12 -0700

    Lazily set up /data directory and updated README

--- notebook-repository/README.md
+++ notebook-repository/README.md
@@ -1,5 +1,27 @@
-> sudo docker network create repo
+# GenePattern Notebook Repository
 
-> sudo docker run --rm --net repo -p 8000:8000 -p 8001:8001 \
->     -v /Users/tabor/PycharmProjects/notebook-docker/genepattern-repository/jupyterhub_config.py:/srv/jupyterhub/jupyterhub_config.py \
->     -v /var/run/docker.sock:/var/run/docker.sock 4751e6ca49f3
+This is a dockerized instance of the GenePattern Notebook Repository, a customized DockerHub deployment
+running the GenePattern Notebook environment.
+
+# Installing the Notebook Repository
+
+To start an instance of the Notebook Repository, first pull both the Notebook Repository and the
+GenePattern Notebook docker containers:
+
+> docker pull genepattern/notebook-repository
+> docker pull genepattern/genepattern-notebook
+
+Next, make sure you have a directory ready on the host file system that will store the persistent state of
+the repository. By default this will be located at `/data`, but this can be overridden (see below).
+
+# Starting the Notebook Repository
+
+We recommend starting the Notebook Repository container using the `start-repository.sh` script included in
+this directory. This will start the container with all of the docker options needed to run. It will also
+lazily set up your data directory, if necessary, and configure docker to launch single-user containers as
+siblings of the Notebook Repository container.
+
+You can override the location of your data directory by passing in the `--DATA` parameter. If you do not,
+the container will assume that it lives at `/data` on the host file system. See the example below.
+
+> ./start-repository.sh --data=/path/to/data
--- notebook-repository/start-repository.sh
+++ notebook-repository/start-repository.sh
@@ -17,14 +17,60 @@ done
 # Assign defaults to arguments
 if [ -z "$DATA" ]
 then
-      DATA=/data
+    DATA=/data
 fi
 
 # Copy variable for second use (bash is a harsh mistress)
 export DATA_DIR=$DATA
 
 # Create the docker network if it does not yet exist
-docker network create repo
+NETWORK_EXISTS=$(docker network inspect repo)
+if ! NETWORK_EXISTS=="[]"
+then
+    docker network create repo
+fi
+
+# Set up the mounted data directory, if necessary
+[[ -d $DATA_DIR/repository ]] || mkdir $DATA_DIR/repository
+[[ -d $DATA_DIR/users ]] || mkdir $DATA_DIR/users
+[[ -d $DATA_DIR/shared ]] || mkdir $DATA_DIR/shared
+[[ -d $DATA_DIR/auth ]] || mkdir $DATA_DIR/auth
+
+export DB_EXISTS=false
+export CONFIG_EXISTS=false
+export SETTINGS_EXISTS=false
+
+[[ -f $DATA_DIR/db.sqlite3 ]] || export DB_EXISTS=true
+[[ -f $DATA_DIR/jupyterhub_config.py ]] || export CONFIG_EXISTS=true
+[[ -f $DATA_DIR/settings.py ]] || export SETTINGS_EXISTS=true
+
+echo $DB_EXISTS
+echo $CONFIG_EXISTS
+echo $SETTINGS_EXISTS
+
+if ! $DB_EXISTS || ! $CONFIG_EXISTS || ! $SETTINGS_EXISTS
+then
+    echo "INSIDE THE BIG IF"
+    docker run --rm --name=copy_data genepattern/notebook-repository &
+    sleep 10
+
+    if [ DB_EXISTS==false ]
+    then
+        docker cp copy_data:/data/db.sqlite3 $DATA_DIR/db.sqlite3
+    fi
+
+    if [ CONFIG_EXISTS==false ]
+    then
+        docker cp copy_data:/data/jupyterhub_config.py $DATA_DIR/jupyterhub_config.py
+    fi
+
+    if [ SETTINGS_EXISTS==false ]
+    then
+        docker cp copy_data:/data/settings.py $DATA_DIR/settings.py
+    fi
+
+    docker stop copy_data
+fi
 
 # Start the Notebook Repository container
-docker run --rm --net=repo --name=notebook-repository -e DATA_DIR=$DATA -p 80:80 -v $DATA_DIR:/data -v /var/run/docker.sock:/var/run/docker.sock genepattern/notebook-repository
+docker run --rm --net=repo --name=notebook_repository -e DATA_DIR=$DATA -p 80:80 -v $DATA_DIR:/data -v /var/run/docker.sock:/var/run/docker.sock genepattern/notebook-repository
