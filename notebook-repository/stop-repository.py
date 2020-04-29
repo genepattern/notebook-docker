8daf492bca1a4028ceb07dad822c01ccb05e2f79
Thorin Tabor <tmtabor@users.noreply.github.com>
2020-04-29 15:52:36 -0400

    Add support for overridden container names

--- notebook-repository/stop-repository.py
+++ notebook-repository/stop-repository.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
 import subprocess
+import argparse
+
+
+##########################################
+# Get the arguments passed to the script #
+##########################################
+
+# Handle the --data and --port options
+parser = argparse.ArgumentParser(description='Stop the Docker container for the Notebook Repository')
+parser.add_argument('-c', '--container', type=str, default='notebook_repository', help='Name given to the container')
+
+# Parse the arguments
+args = parser.parse_args()
+
+##########################################
+# Stop the container                     #
+##########################################
 
 try:
-    subprocess.Popen('docker stop notebook_repository'.split())
+    subprocess.Popen(f'docker stop {args.container}'.split())
 except KeyboardInterrupt:
     print('Shutting down Notebook Repository')
