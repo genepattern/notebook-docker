3c5171892891596115c0f8942b38e37758a049dd
tmtabor <tmtabor@cloud.ucsd.edu>
2021-06-07 09:40:34 -0700

    Adding streamlined Python 3.9 container

---
+++ notebook-python39/README.md
@@ -0,0 +1,8 @@
+![docker pulls](https://img.shields.io/docker/pulls/genepattern/genepattern-notebook.svg) 
+![docker stars](https://img.shields.io/docker/stars/genepattern/genepattern-notebook.svg)
+
+This is a Docker image containing the full [GenePattern Notebook environment](https://notebook.genepattern.org).
+
+# Build Instructions
+> cd notebook-docker/genepattern-notebook  
+> docker build -t genepattern/genepattern-notebook:<version_number> .
---
+++ notebook-python39/custom.js
@@ -0,0 +1,142 @@
+/**
+ * Use require.js to load the static GenePattern Notebook files
+ */
+require(['base/js/namespace', 'jquery'], function(Jupyter, $) {
+    "use strict";
+
+    // Rename default kernel
+   $("ul#new-menu > [id^='kernel-'] > a").text('Notebook');
+
+    // Attach the workspace name, if available
+    function attach_workspace_name() {
+        // Get the project directory name
+        const parts = location.pathname.split('/');
+        const dir_name = parts.length >= 4 ? parts[3] : null;
+
+        $.ajax({
+            method: 'GET',
+            url: `/hub/user.json`,
+            success: (user) => {
+				user = JSON.parse(user);
+				if (user.projects) user.projects.forEach(project => {
+					if (dir_name === project.name)
+					    $("#header").find(".flex-spacer").html(
+					        $(`<p class="gp-project"><a href="/hub/home" target="_blank">Projects</a> &raquo; ${project.metadata.name}</p>`)
+                        );
+                });
+            },
+            error: () => console.log('Workspace name not available')
+        })
+    }
+
+   // Decode username
+    const decode_username = (u) => decodeURIComponent(u.replace(/-/g, '%'));
+
+    function cookie_to_map() {
+        const cookie_map = {};
+
+        document.cookie.split(';').forEach(function(cookie_str) {
+            const pair = cookie_str.split('=');
+            const key = pair[0].trim();
+            cookie_map[key] = pair.length > 1 ? pair[1].trim() : '';
+        });
+
+        return cookie_map;
+    }
+
+    function extract_username() {
+        let username = null;
+
+        // Try to get username from GPNB cookie
+        const cookie_map = cookie_to_map();
+        if (cookie_map['gpnb-username'] !== undefined &&
+            cookie_map['gpnb-username'] !== null &&
+            cookie_map['gpnb-username'] !== 'undefined' &&
+            cookie_map['gpnb-username'] !== 'null') {
+            username = cookie_map['gpnb-username'];
+        }
+
+        // Try to get username from JupyterHub cookie
+        if (username === null) {
+            $.each(cookie_map, function(i) {
+                if (i.startsWith("jupyter-hub-token-")) {
+                    username = decodeURIComponent(i.match(/^jupyter-hub-token-(.*)/)[1]);
+                }
+            });
+        }
+
+        // Try to get the username from the URL
+        if (username === null) {
+            const url_parts = window.location.href.split('/');
+            if (url_parts.length >= 5 &&
+                url_parts[0] === window.location.protocol &&
+                url_parts[1] === '' &&
+                url_parts[2] === window.location.host &&
+                url_parts[3] === 'user') {
+                username = decodeURI(url_parts[4])
+            }
+        }
+
+        // If all else fails, prompt the user
+        if (username === null) {
+            username = prompt("What is your username?", "");
+        }
+
+        // Set a GPNB cookie
+        document.cookie = 'gpnb-username' + '=' + username;
+
+        return username;
+    }
+
+    // Add username to the logout button
+    const username =  extract_username();
+    if (username) $('#logout').html( "Logout " + decode_username(username));
+
+    // Add the help button to the header
+    $("span > a.btn[href='/hub/home']").css("margin-right", "2px"); // Fix spacing
+    $("#header-container").append(
+        $('<span><a href="https://notebook.genepattern.org" target="_blank" class="btn btn-default btn-sm navbar-btn pull-right" style="margin-right: 4px;">Help</a></span>')
+    );
+
+	// Attach the loading screen
+	const loadingScreen = function() {
+	    const base_url = Jupyter.contents ? Jupyter.contents.base_url : (Jupyter.notebook_list ? Jupyter.notebook_list.base_url : Jupyter.editor.base_url);
+	    const STATIC_PATH = location.origin + base_url + "nbextensions/genepattern/resources/";
+
+		return $("<div></div>")
+			.addClass("loading-screen")
+			.append("Please wait while GenePattern Notebook is loading...")
+			.append($("<br/><br/>"))
+			.append(
+				$("<img/>")
+					.attr("src", STATIC_PATH + "gp-logo.png")
+			);
+	};
+
+    // Add the loading screen if this is a notebook
+    if (Jupyter.notebook) {
+        $("body").append(loadingScreen());
+    }
+
+    // Fade the loading screen when the kernel is ready
+    $([Jupyter.events]).on('kernel_ready.Kernel', function() {
+        $(".loading-screen").hide("fade");
+    });
+
+    // Backup attempt to fade loading screen
+    setTimeout(function () {
+        $(".loading-screen").hide("fade");
+    }, 5000);
+
+    // Add the project name
+    attach_workspace_name();
+
+    // Bug fix for rename dialog
+    $("#notebook_name").click(function() {
+        setTimeout(function() {
+            Jupyter.keyboard_manager.register_events($(".modal-dialog input[type=text]"));
+        }, 500);
+    });
+
+    console.log("GenePattern Notebook Repository code loaded.");
+});
